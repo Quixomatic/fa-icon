b625ab62d65acac1bab624e8ae27a3d78728faa4
James Freund <jfreund18@gmail.com>
2020-10-06 20:20:20 -0400

    - Added icon name property - Property added so that you can dynamically import an icon - This way you can avoid manually imports for each icon definition - Commit gives the option for `icon={'some-icon'}` or `def={iconDefObj}`

---
+++ src/x-saw-fa-icon-demo/components/fa-icon/fa-icon.js
@@ -0,0 +1,27 @@
+import { createCustomElement } from '@servicenow/ui-core';
+import snabbdom from '@servicenow/ui-renderer-snabbdom';
+
+import styles from './styles.scss';
+import view from './view';
+import actions from './actions';
+
+createCustomElement('fa-icon', {
+	renderer: { type: snabbdom },
+	view,
+	properties: {
+		def: {
+			default: null,
+		},
+		icon: {
+			default: null,
+		},
+		size: {
+			default: 'md'
+		},
+		spin: {
+			default: false
+		},
+	},
+	styles,
+	...actions,
+});
--- src/x-saw-fa-icon-demo/view.js
+++ src/x-saw-fa-icon-demo/view.js
@@ -19,11 +19,19 @@ export default (state, { updateState, updateProperties, dispatch }) => {
 				<div className="inner-header"></div>
 				<div className="inner-body">
 					<div className="body-icons">
-						{iconsArray.map((iconObj) => {
+						{iconsArray.map((iconObj, idx) => {
 							return (
 								<div className="icon-container">
 									<div className="icon-body">
-										<fa-icon def={iconObj} size="xl" />
+										{idx % 2 === 0 ? (
+											//! You can either load icons directly with a definition object
+											//! ie import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+											<fa-icon def={iconObj} size="xl" />
+										) : (
+											//! Or  you can dynamically load the icon with its name.
+											//! ie icon={'arrow-right'}
+											<fa-icon icon={iconObj.iconName} size="xl" />
+										)}
 									</div>
 									<div className="icon-footer">
 										<span>{iconObj.iconName}</span>
