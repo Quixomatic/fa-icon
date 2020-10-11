af6caf3fc05e5a1af1cb43e2a839e2ca4c668581
James Freund <jfreund18@gmail.com>
2020-10-11 09:53:52 -0400

    - Added ability to set prefix in constants.js file - Removed async as it adds a huge amount of compile time - Removed host specific styling - Added componentId as property to set custom ids

---
+++ src/x-saw-fa-icon-demo/components/fa-icon/constants.js
@@ -0,0 +1,3 @@
+//! Set this to change this component to whatever custom prefix you want
+//! eg: <nowcomponents-fa-icon />
+export const prefix = 'nowcomponents';
--- src/x-saw-fa-icon-demo/constants.js
+++ src/x-saw-fa-icon-demo/constants.js
@@ -2,4 +2,4 @@ import { customAlphabet } from 'nanoid';
 
 export const getNanoID = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', 32);
 
-export const componentUniqueID = '@saleswon/fa-icon-demo';
+export const componentUniqueID = '@nowcomponents/fa-icon-demo';
